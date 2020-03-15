b2c274bff2b6b9e86f532746f4eb2b7f7b4f1803
Diego Vega <diegovr99@hotmail.com>
2020-03-15 15:59:56 -0600

    Primer crud DONE

--- ejemplo/urls.py
+++ ejemplo/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('ejemplo/agregarObjeto.html', views.formularioAgregarObjeto),
     path('ejemplo/listaObjeto.html', views.lista_objetos),
+    path('editarObjeto/<int:objeto_id>', views.editarObjeto),
+    path('eliminarObjeto/<int:objeto_id>', views.eliminarObjeto),
 ]
--- ejemplo/views.py
+++ ejemplo/views.py
@@ -17,3 +17,19 @@ def formularioAgregarObjeto(request):
 def lista_objetos(request):
 	objetos = objeto.objects.all()
 	return render(request, 'ejemplo/listaObjeto.html', {'objetos': objetos})
+
+def editarObjeto(request, objeto_id):
+	objetos = objeto.objects.get(id = objeto_id)
+	if request.method=="POST":
+		formulario = FormularioObjeto(request.POST, instance=objetos)
+		if formulario.is_valid():
+			formulario.save()
+			return render(request, "ejemplo/editarObjeto.html", {"formulario":formulario})
+	else:
+		formulario = FormularioObjeto(instance=objetos)
+		return render(request, "ejemplo/editarObjeto.html", {"formulario":formulario})
+
+def eliminarObjeto(request, objeto_id):
+	objetos = objeto.objects.get(id = objeto_id)
+	objetos.delete()
+	return render(request, '/ejemplo/index.html')
